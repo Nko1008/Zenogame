256e361c68b49d203fb4a8a72c745149b1522e82
MymyBelles <pro.amymagri@gmail.com>
2026-09-21 09:28:13 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type Character struct {
+	Name      string
+	Class     string
+	Level     int
+	MaxHealth int
+	Health    int
+	Inventory []string
+}
+
+func initCharacter() Character {
+	player := Character{
+		Name:      "Zeno",
+		Class:     "Elfe",
+		Level:     1,
+		MaxHealth: 100,
+		Health:    40,
+		Inventory: []string{"Potion", "Potion", "Potion"},
+	}
+	return player
+}
+
+func displayCharacter(player Character) {
+	player.Name = "Zeno"
+	player.Class = "Elfe"
+	player.Level = 1
+	player.MaxHealth = 100
+	player.Health = 40
+	player.Inventory = []string{"Potion", "Potion", "Potion"}
+	fmt.Println("Nom :", player.Name)
+	fmt.Println("Classe :", player.Class)
+	fmt.Println("Niveau :", player.Level)
+	fmt.Println("Santé maximale :", player.MaxHealth)
+	fmt.Println("PV :", player.Health, "/", player.MaxHealth)
+	fmt.Println("Inventaire :", player.Inventory)
+}
+
+func main() {
+	player := initCharacter()
+
+	displayCharacter(player)
+}
