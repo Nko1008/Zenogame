865d4b3e3f2d9d490894b80699ce124c84faecfc
Nko1008 <norhann.koraichi@ynov.com>
2026-09-22 12:22:16 +0200

    Add potion functions for healing and poisoning

---
+++ src/potion.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Tâche 5 : la potion se consomme et rend 50 PV, sans dépasser le max.
+func takePot(c *Character) {
+	if !removeInventory(c, "Potion de vie", 1) {
+		fmt.Println("Vous n'avez pas de Potion de vie.")
+		return
+	}
+	fmt.Println("Vous utilisez une Potion de vie.")
+	c.Health += 50
+	if c.Health > c.MaxHealth {
+		c.Health = c.MaxHealth
+	}
+	fmt.Printf("PV : %d / %d\n", c.Health, c.MaxHealth)
+}
+
+// Tâche 9 : 10 dégâts par seconde pendant 3 secondes.
+func poisonPot(c *Character) {
+	if !removeInventory(c, "Potion de poison", 1) {
+		fmt.Println("Vous n'avez pas de Potion de poison.")
+		return
+	}
+	fmt.Println("Vous buvez une Potion de poison...")
+	for i := 0; i < 3; i++ {
+		time.Sleep(1 * time.Second)
+		c.Health -= 10
+		if c.Health < 0 {
+			c.Health = 0
+		}
+		fmt.Printf("PV : %d / %d\n", c.Health, c.MaxHealth)
+	}
+	isDead(c)
+}
